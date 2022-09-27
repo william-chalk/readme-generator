438a0f9367b6515cff24c1378ad4a73448401b3e
William Chalk <3halk1@gmail.com>
2022-09-26 20:04:46 -0400

    added user input and file creation

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,111 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkDown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const { rejects } = require("assert");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the ReadMe? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a title for the ReadMe!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description for your ReadMe (Required)",
+    validate: (desInput) => {
+      if (desInput) {
+        return true;
+      } else {
+        console.log("Please enter a description for your ReadMe!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "install",
+    message:
+      "Please provide inscructions on how to install your project (Required)",
+    validate: (installInput) => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log(
+          "Please provide instructions on how to install your project!"
+        );
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide the usage of your project (Required)",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please provided the usage of your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "confirm",
+    name: "confirmCredits",
+    message: "Would you like to add collaborators for your project?",
+    default: true,
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Provide the names and GitHub links for your collaborators",
+    when: ({ confirmCredits }) => {
+      if (confirmCredits) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/README.md", data, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "File Created!",
+      });
+    });
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(readMeData) {
+  return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+  .then((userData) => {
+    return generateMarkDown(userData);
+  })
+  .then((generateFile) => {
+    return writeToFile(generateFile);
+  });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,50 @@
+const fs = require("fs");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  return; //license badge based on which license what passed in;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  return; //license link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  return; //license section of read me ;
+}
+
+const generateCredit = (creditArr) => {
+  if (!creditArr) {
+    return "";
+  }
+  return `
+    ## Credits
+    ${creditArr}`;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+          ##Description
+          ${data.description}
+          ##Installation
+          ${data.install}
+          ##Usage
+          ${data.usage}
 `;
 }
 
